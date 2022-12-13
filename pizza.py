89b3a76a60c4f63ff6acc8f017b6f99a759c0dc6
vlad_kuzmin <11651999-vlad_kuzmin@users.noreply.gitlab.com>
2022-12-13 10:51:05 +0400

    Compeate a pizzeria project (sorry for the disgusting commit)

--- pizza.py
+++ pizza.py
@@ -1,11 +1,72 @@
+import time
+from datetime import datetime, timedelta
+
+
 class AbstractPizza:
-	pass
+    """Базовый класс пиццы от которого наследуются все остальные"""
+
+    def __init__(self):
+        """
+        Этот метод срабатывает тогда когда, создается экземпляр класса, он объявляет
+        переменные которые относятся к классу, далее доступ к ним осуществляется через .self
+        """
+
+        self.title = ''
+        self.dough = ''
+        self.sauce = ''
+        self.filing = ''
+        self.price = 0
+        self.cooking_time = 0
+
+    def time(self, start_time):
+        now = datetime.now()
+        delta = now - start_time
+        if delta.seconds / 60 > self.cooking_time:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        """Метод представления экземпляра класса в строковом типе данных."""
+        # объявляем его в родительском классе далее переиспользуем в дочерних.
+
+        return self.title
+
 
 class PepperoniPizza(AbstractPizza):
-	pass
+
+    def __init__(self):
+        super().__init__()  # тут мы вызываем родительский метод __init__ что бы
+        # в дочерний класс передались переменные и методы родительского
+        # super() - это обращение к классу от которого наследуемся
+        self.title = 'Пепперони'
+        self.dough = 'Дрожжевое'
+        self.sauce = 'Томатный'
+        self.filing = 'Помидоры, оливки, колбаса, перец'
+        self.price = 400
+        self.cooking_time = 1
+
 
 class BarbecuePizza(AbstractPizza):
-	pass
+
+    def __init__(self):
+        super().__init__()  # опять достаем все из класса от которого наследуемся
+        self.title = 'Барбекю'
+        self.dough = 'Дрожжевое'
+        self.sauce = 'Барбекю '
+        self.filing = 'Помидоры, оливки, куриная грудка'
+        self.price = 450
+        self.cooking_time = 2
+
 
 class SeafoodPizza(AbstractPizza):
-	pass
+
+    def __init__(self):
+        super().__init__()  # опять достаем все из класса от которого наследуемся
+        self.title = 'Дары моря'
+        self.dough = 'Слоёное'
+        self.sauce = 'Майонез'
+        self.filing = 'Помидоры, осьминог, креветки, мидии'
+        self.price = 450
+        self.cooking_time = 25
+
--- seller.py
+++ seller.py
@@ -1,2 +1,35 @@
+from datetime import datetime
+
+
 class Seller:
-	pass
+	def __init__(self):
+		self.full_name = None
+		self.password = None
+		self.time_open = None
+		self.time_close = None
+
+	def registrate(self, full_name, password):
+		self.full_name = full_name
+		self.password = password
+		return f"Продавец: {full_name} зарегистрирован."
+
+	def open(self, terminal):
+		self.time_open = datetime.now()
+		terminal.opened = True
+		print("Кассы открыты, терминал работает.")
+
+	def close(self, terminal):
+		self.time_close = datetime.now()
+		terminal.opened = False
+		print("Кассы закрыты, терминал не работает.")
+
+	def __str__(self):
+		info = f"Продавец: {self.full_name}"
+		return info
+
+
+
+
+
+
+
